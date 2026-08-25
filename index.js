f4a542c10fbaa63f14695c819ab26a6710e826ab
Zibnu <crimhomid@gmail.com>
2026-08-25 15:30:09 +0700

    update code in index.js

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require("express");
-
-const app = express();
-app.use(express.json());
+const bot = require("./src/bot/bot.js");
+const webhook = require("./src/routes/webhookRoute.js");
 require("dotenv").config();
+const app = express();
 
+app.use(express.json());
+app.use(webhook);
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "success",
-    })
+    res.send("Server Bot AI is Runing");
 })
 
 const PORT = process.env.PORT;
 if(process.env.NODE_ENV !== "productin") {
-    app.listen(PORT, () => console.log("Server is Running"));
+    app.listen(PORT, async () => {
+        console.log("Server Is Runing");
+
+        if(process.env.WEBHOOK_URL) {
+            const fullWebhookUrl = `${process.env.WEBHOOK_URL}/telegram-webhook`;
+            await bot.api.setWebhook(fullWebhookUrl);
+            console.log(`Webhook telegram di daftarkan ke ${fullWebhookUrl}`);
+        }
+    });
 }
