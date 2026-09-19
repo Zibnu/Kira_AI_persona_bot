1ca4aeb7f0e5457b5ee7d12023462982d44725b7
Zibnu <crimhomid@gmail.com>
2026-09-19 23:12:29 +0700

    update model AI

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -4,7 +4,7 @@ async function generateAIResponse({ botName, botPersona, userName, userBio, hist
     let limitedHistory = (history.length > 10) ? history.slice(-10) : (history || []);
 
     const chat = genAI.chats.create({
-        model: "gemini-3.1-flash-lite",
+        model: "gemini-3.5-flash",
         history: limitedHistory,
         config: {
             systemInstruction: `                                                                                                                                  
